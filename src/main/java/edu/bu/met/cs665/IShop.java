aea6a91d4f03840be5a57a8ab6360215b23e9611
jonorgan <jonorgan26@gmail.com>
2023-10-03 15:23:55 -0400

    first commit

---
+++ src/main/java/edu/bu/met/cs665/IShop.java
@@ -0,0 +1,16 @@
+/**
+ * Name: Jon Organ
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 10/3/2023
+ * File Name: IShop.java
+ * Description: This file contains the IShop interface to be implemented by the shop class. It contains
+ * the methods registerObserver, removeObserver, and notifyObservers.
+ */
+
+package hw2;
+
+interface IShop {
+	public registerObserver();
+	public removeObserver();
+	public notifyObservers();
+}
---
+++ src/main/java/edu/bu/met/cs665/Shop.java
@@ -0,0 +1,27 @@
+/**
+ * Name: Jon Organ
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 10/3/2023
+ * File Name: Shop.java
+ * Description: This file contains the Shop concrete class which implements the IShop interface. It contains
+ * the methods registerObserver, removeObserver, and notifyObservers.
+ */
+
+package hw2;
+
+public class Shop {
+    public void registerObserver()
+    {
+
+    }
+
+    public void removeObserver()
+    {
+
+    }
+
+    public void notifyObserver()
+    {
+        
+    }
+}

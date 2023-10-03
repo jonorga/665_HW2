7dfb69e5e14dbbfb7d555b213527fd14e3965e83
jonorgan <jonorgan26@gmail.com>
2023-10-03 15:48:50 -0400

    All main classes created

---
+++ src/main/java/edu/bu/met/cs665/DeliveryRequest.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Jon Organ
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 10/3/2023
+ * File Name: DeliveryRequest.java
+ * Description: This file contains the Delivery Request class
+ */
+
+package hw2;
+
+public class DeliveryRequest() {
+    
+}
